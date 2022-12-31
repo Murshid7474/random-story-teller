642cc1a0ba122f44eaf6541d06c609f30b600468
MOHAMMED MURSHID ALAM <83682620+Murshid7474@users.noreply.github.com>
2022-12-31 09:22:26 +0530

    Update randomstoryteller.py

--- randomstoryteller.py
+++ randomstoryteller.py
@@ -1,6 +1,8 @@
+#random story teller
 import random
 when=['Yesterday','Long back','In future','Before you were born']
 who=['Batman','Iron man','Spider man','Ant man','Murshid']
 went=['Bihar','Bangalore','Delhi','Jaipur']
 what=['to eat a lot of cake' , 'to safe his fans' ,'to defeat the enemy' , 'to bright the future']
 print(random.choice(when)+'.'+random.choice(who),'went to',random.choice(went)+'.'+random.choice(what))
+#thank you for watching it
